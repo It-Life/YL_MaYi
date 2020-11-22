ea283e9cb973d323f60773ab174ff401f510bacd
onion <652597991@qq.com>
2020-11-22 20:26:58 +0800

    Dx
    初步原型图

---
+++ YClient/YDome/YAppletDome/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		主页	
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '主页'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
